d20200bcf2770bec008d6e50b8488a375fd41bfd
rkpust <rezaul.cse.pust17@gmail.com>
2023-07-30 22:08:05 +0600

    added sun icon and header text in SiteNavigation component

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-    </div>
+    <header class="sticky top-0 bg-weather-primary shadow-lg">
+        <nav class="container flex flext-col sm:flex-row items-center gap-4 text-white py-6">
+             <div class="flex items-center gap-3 flex-1">
+                <i class="fa-solid fa-sun text-2xl"></i>
+                <p class="text-2xl">The Local Weather</p>
+            </div>
+        </nav>
+    </header>
 </template>
 
 <script setup>
