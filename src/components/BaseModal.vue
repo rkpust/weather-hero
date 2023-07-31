099695ce36eaf7e72b15caa4f7c58d6b03587721
rkpust <rezaul.cse.pust17@gmail.com>
2023-07-31 09:34:59 +0600

    added BaseModal as toggable

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8">
-        <div class="p-4 bg-white self-start mt-32 max-w-screen-md rounded-md">
+    <div
+        v-show="modalActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+    >
+        <div
+            v-if="modalActive"
+            class="p-4 bg-white self-start mt-32 max-w-screen-md rounded-md"
+        >
             <slot />
             <button class="text-white mt-8 bg-weather-primary py-2 px-6">Close</button>
         </div>
@@ -8,5 +14,10 @@
 </template>
 
 <script setup>
-
+defineProps({
+    modalActive: {
+        type: Boolean,
+        default: false,
+    },
+});
 </script>
