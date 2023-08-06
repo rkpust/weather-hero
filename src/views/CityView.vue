141c197244749a750b1577be1dc67f6fb9691178
rkpust <rezaul.cse.pust17@gmail.com>
2023-08-06 13:52:47 +0600

    created AsyncCityView and called openweather api & calculated

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
+        <AsyncCityView />
         <h1>Hello From CityView</h1>
     </div>
 </template>
 
 <script setup>
-
+import AsyncCityView from '../components/AsyncCityView.vue';
 </script>
