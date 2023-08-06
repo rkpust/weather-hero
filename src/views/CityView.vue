5ac9a6fce9b043427f25e458b32cf9264b4076bc
rkpust <rezaul.cse.pust17@gmail.com>
2023-08-06 14:32:13 +0600

    added Suspense built-in component for async data loading

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <AsyncCityView />
+        <Suspense>
+            <AsyncCityView />
+            <template #fallback>
+                <p class="text-center text-white py-24">Loading...</p>
+            </template>
+        </Suspense>
         <h1>Hello From CityView</h1>
     </div>
 </template>
