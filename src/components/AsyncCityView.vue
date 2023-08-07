c66076ba8be8eb9fb0d58b3c7385e882eedbac87
rkpust <rezaul.cse.pust17@gmail.com>
2023-08-07 22:13:46 +0600

    added hourly weather

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -45,6 +45,7 @@
         </div>
 
         <hr class="border-white border-opacity-10 border w-full"/>
+        <!-- More Weather Info-->
 
         <div class="flex flex-col items-center text-white text-center py-12">
             <h2 class="text-2xl mb-2">More Info</h2>
@@ -68,6 +69,33 @@
                     &nbsp;
                     Wind Degree: {{ weatherData.current.wind_deg }}
                 </p>
+        </div>
+
+        <hr class="border-white border-opacity-10 border w-full"/>
+        <!-- Hourly Weather-->
+        <div class="max-w-screen-md w-full py-12">
+            <div class="mx-8 text-white">
+                <h2 class="text-2xl text-center mb-4">Hourly Weather</h2>
+                <div class="flex gap-10 overflow-x-scroll">
+                    <div v-for="hourData in weatherData.hourly" :key="hourData.dt" class="flex flex-col gap-4 items-center">
+                        <p class="whitespace-nowrap text-md">
+                            {{
+                                new Date(hourData.currentTime).toLocaleTimeString(
+                                    "en-us",
+                                    { hour: "numeric",
+                                })
+                            }}
+                        </p>
+                        <img
+                            class="w-auto h-[50px] object-cover"
+                            :src=" `http://openweathermap.org/img/wn/${hourData.weather[0].icon}@2x.png` "
+                        />
+                        <p class="text-xl">
+                            {{ Math.round(hourData.temp) }}&deg;
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
