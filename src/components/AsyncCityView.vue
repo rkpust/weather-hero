0b8a83a171b04cf56bc779ecf5fc40940e54d134
rkpust <rezaul.cse.pust17@gmail.com>
2023-08-08 01:01:40 +0600

    added 7 day forecast

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -101,6 +101,36 @@
                 </div>
             </div>
         </div>
+
+        <hr class="border-white border-opacity-10 border w-full"/>
+        <!-- Daily Weather-->
+        <div class="max-w-screen-md w-full py-12">
+            <div class="mx-8 text-white">
+                <h2 class="text-2xl text-center mb-4">7 Day Forecast</h2>
+                <div 
+                    v-for="day in weatherData.daily" 
+                    :key="day.dt"
+                    class="flex items-center"
+                    >
+                    <p class="flex-1">
+                        {{ new Date(day.dt * 1000).toLocaleDateString(
+                            "en-us",
+                            {
+                                weekday: "long",
+                            }
+                        ) }}
+                    </p>
+                    <img
+                    class="w-[50px] h-[50px] object-cover"
+                    :src=" `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png` "
+                    />
+                    <div class="flex gap-2 flex-1 justify-end">
+                        <p>H: {{ Math.round(day.temp.max) }}&deg;</p>
+                        <p>L: {{ Math.round(day.temp.min) }}&deg;</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
