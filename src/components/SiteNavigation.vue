d2a00549a3bf71c011e0d3c22e14810e406ef318
rkpust <rezaul.cse.pust17@gmail.com>
2023-08-10 02:05:58 +0600

    added query id in router when clicked in plus icon

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -83,6 +83,7 @@ const addCity = () => {
 
     let query = Object.assign({}, route.query);
     delete query.preview;
+    query.id = locationObj.id;
     router.replace({ query })
     
     console.log(saveCities.value);
