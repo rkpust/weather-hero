fa6df994d3dd633be37623635668f3b4cf070353
rkpust <rezaul.cse.pust17@gmail.com>
2023-08-10 17:18:33 +0600

    spelling correction of class name

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="sticky top-0 bg-weather-primary shadow-lg z-10">
-        <nav class="container flex flext-col sm:flex-row items-center gap-4 text-white py-6">
+        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
              <RouterLink :to="{ name: 'home'}">
                  <div class="flex items-center gap-3">
                     <i class="fa-solid fa-sun text-2xl"></i>
