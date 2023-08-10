7a4ddb4b94ccacc0f7830fc9a8aa8c299260f234
rkpust <rezaul.cse.pust17@gmail.com>
2023-08-11 00:22:46 +0600

    created TheFooter component and added in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <SiteNavigation />
     <RouterView />
+    <TheFooter />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
 import SiteNavigation from './components/SiteNavigation.vue';
+import TheFooter from './components/TheFooter.vue';
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-weather-secondary mt-8">
+        <footer class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+            <p class="text-center">Developed By <a href="https://www.linkedin.com/in/rkpust/" target="_blank" class="hover:text-green-500">Rezaul Karim</a></p>
+            <p class="text-center">Courtesy By <a href="https://openweathermap.org/" target="_blank" class="hover:text-green-500">OpenWeather</a></p>
+            <p class="text-center">All Rights Reserved &copy; 2023</p>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+
+</script>
